b72fcffd996ad46753eedbcdbca2035d575552f7
William Hou <williamhou38@yahoo.com>
2024-09-05 00:54:36 -0500

    new changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,77 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClient, HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { TypeaheadModule } from 'ngx-bootstrap/typeahead';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SummaryPipe } from './pipes/summary.pipe';
+import { JwtInterceptor } from './interceptors/jwt.interceptor';
+import { ErrorInterceptor } from './interceptors/error.interceptor';
+import { ToastrModule } from 'ngx-toastr';
+import { BlogCardComponent } from './components/blog-components/blog-card/blog-card.component';
+import { BlogEditComponent } from './components/blog-components/blog-edit/blog-edit.component';
+import { BlogComponent } from './components/blog-components/blog/blog.component';
+import { BlogsComponent } from './components/blog-components/blogs/blogs.component';
+import { FamousBlogsComponent } from './components/blog-components/famous-blogs/famous-blogs.component';
+import { CommentBoxComponent } from './components/comment-components/comment-box/comment-box.component';
+import { CommentSystemComponent } from './components/comment-components/comment-system/comment-system.component';
+import { CommentsComponent } from './components/comment-components/comments/comments.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { PhotoAlbumComponent } from './components/photo-album/photo-album.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SummaryPipe,
+    BlogComponent,
+    BlogCardComponent,
+    BlogEditComponent,
+    BlogsComponent,
+    FamousBlogsComponent,
+    CommentBoxComponent,
+    CommentSystemComponent,
+    CommentsComponent,
+    DashboardComponent,
+    HomeComponent,
+    LoginComponent,
+    NavbarComponent,
+    NotFoundComponent,
+    PhotoAlbumComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-right'
+    }),
+    BsDropdownModule.forRoot(),
+    CollapseModule.forRoot(),
+    TypeaheadModule.forRoot(),
+    CarouselModule.forRoot(),
+    PaginationModule.forRoot()
   ],
-  providers: [],
+  providers: [
+    HttpClient,
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+    provideHttpClient(withInterceptorsFromDi())
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
